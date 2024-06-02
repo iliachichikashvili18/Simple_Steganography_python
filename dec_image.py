fc53769dfb8d2b609a274eb52a9f2fae444c92a4
iliachichikashvili18 <165050119+iliachichikashvili18@users.noreply.github.com>
2024-06-02 21:10:34 +0400

    Add files via upload

---
+++ dec_image.py
@@ -0,0 +1,38 @@
+from PIL import Image
+
+def decrypt_image(original_image_path, encrypted_image_path):
+    # open the original and encrypted images
+    original_image = Image.open("xdd.jpg")
+    encrypted_image = Image.open("zd.png")
+
+    # load pixel maps
+    original_pixel_map = original_image.load()
+    encrypted_pixel_map = encrypted_image.load()
+
+    # get the size of the images
+    width, height = original_image.size
+
+    # space for empy string
+    decrypted_message = ""
+
+    # iterate over each pixel in the images
+    for y in range(height):
+        for x in range(width):
+            # get RGB values of the original and encrypted pixels
+            r_original, g_original, b_original = original_pixel_map[x, y]
+            r_encrypted, g_encrypted, b_encrypted = encrypted_pixel_map[x, y]
+
+            # decrypt the message by subtracting the RGB values(at this moment only red)
+            decrypted_char = chr((r_encrypted - r_original)+96)
+
+            # append the decrypted character to the message
+            if decrypted_char != '`':
+                decrypted_message += decrypted_char
+
+    return decrypted_message
+
+# usage
+original_image_path = "xdd.jpg"
+encrypted_image_path = "zd.png"
+decrypted_message = decrypt_image(original_image_path, encrypted_image_path)
+print("Decrypted Message:", decrypted_message)
---
+++ enc_image.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+# open the image
+img = Image.open("kai.png")
+
+# load the pixel map
+pixel_map = img.load()
+
+# define the string
+text = "ASCII stands for American Standard Code for Information Interchange. Below is the ASCII character table, including descriptions of the first 32 characters. ASCII was originally designed for use with teletypes, and so the descriptions are somewhat obscure and their use is frequently not as intended."
+
+# define the starting point
+xd = 0
+
+# iterate over each character in the string
+for char in text:
+    # get the RGB values of the current pixel
+    r, g, b = img.getpixel((xd, 0))
+
+    # convert the character to its ASCII value and add it to the red component
+
+    r = min(r + ord(char)-96 , 255)
+
+    # update the pixel with the new RGB values
+    pixel_map[xd, 0] = (r, g, b)
+
+    # increment the coordinate for the next pixel
+    xd += 1
+
+# save the modified image in a lossless format (PNG)
+img.save("zd.png", format="PNG")
